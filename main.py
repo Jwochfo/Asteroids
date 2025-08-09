e58381d5a058ce113930dab9d817fff284bcc1dc
ftzuse <max10stibo@gmail.com>
2025-08-09 11:10:01 +0200

    CH2 Player finished

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    running = True
-    while running:
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updateable.update(dt)
+
         screen.fill((0,0,0))
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        #limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
+        
+
 
 
 if __name__ == "__main__":
